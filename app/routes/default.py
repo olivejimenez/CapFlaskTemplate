7bdf72000c7aeaf9e4467e4e4348b8dafcdfd279
olivejimenez <89043893+olivejimenez@users.noreply.github.com>
2022-05-13 12:22:07 -0700

    djnflkdfj

--- app/routes/default.py
+++ app/routes/default.py
@@ -17,3 +17,11 @@ def post():
 @app.route('/movielist')
 def movielist():
     return render_template('movielist.html')
+
+@app.route('/cleofrom5to7')
+def cleofrom5to7():
+    return render_template('cleofrom5to7.html')
+
+@app.route('/cigarettesandcoffee')
+def cigarettesandcoffee():
+    return render_template('cigsandcoffee.html')
